97573dc1a9fde31eeb81f07d8d4f159f256ba716
静默 <cau1403090523@gmail.com>
2023-12-21 22:12:06 +0800

    目前很顺利

--- waimai/admin-app/src/main/java/org/hzz/ApiApplication.java
+++ waimai/admin-app/src/main/java/org/hzz/ApiApplication.java
@@ -1,5 +1,6 @@
 package org.hzz;
 
+import org.hzz.dao.BaseRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,7 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "org.hzz.dao")
+// 自定义的实现BaseRepositoryFactoryBean,能够在BaseRepository里面实现自己定义的方法，不用jpa识别
+@EnableJpaRepositories(basePackages = "org.hzz.dao",repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
 @EntityScan(basePackages = "org.hzz.bean.entity")
 @EnableJpaAuditing
 public class ApiApplication {
